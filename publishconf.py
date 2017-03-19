d28f4f87416e5fa1e686e33ba9b2d1b4ea6a3b22
Alexander Popiak <alexander.popiak@gmail.com>
2017-03-19 17:21:03 +0100

    adjust site url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://taman-dan-pantai.github.com'
+SITEURL = 'https://apopiak.github.io/taman-dan-pantai'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
